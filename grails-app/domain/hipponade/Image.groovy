66e8220aa388385da166cfd0499d915162b7c9a8
Mathis Neumann <mne@informatik.uni-kiel.de>
2014-07-08 14:59:03 +0200

    remove Image domain dependencies

--- grails-app/domain/hipponade/Image.groovy
+++ grails-app/domain/hipponade/Image.groovy
@@ -2,18 +2,11 @@ package hipponade
 
 class Image {
     String fileName
-    Recipe recipe
-    Event event
-    Product product
-
     //static belongsTo = [Product, Recipe]
    // static hasOne = [recipe: Recipe, event: Event, product: Product]
     static constraints = {
         // Limit upload file size to 2MB
         fileName blank: false, nullable: false
-        recipe nullable: true
-        event nullable: true
-        product nullable: true
     }
 
     def beforeDelete() {
