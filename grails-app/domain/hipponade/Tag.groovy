7584d5eb759d4131ac46c7b62728ec58857e0287
Mathis Neumann <mne@informatik.uni-kiel.de>
2014-07-08 15:29:36 +0200

    fixed wrong hasMany

--- grails-app/domain/hipponade/Tag.groovy
+++ grails-app/domain/hipponade/Tag.groovy
@@ -3,7 +3,7 @@ package hipponade
 class Tag {
     String label
 
-    static hasMany = [events: Event] //, news: News, products: Product, recipes: Recipe]
+//    static hasMany = [events: Event] //, news: News, products: Product, recipes: Recipe]
 //    static belongsTo = [Product, Recipe, News, Event]
     static constraints = {
         label blank: false, unique: true, minSize: 3, maxSize: 20
