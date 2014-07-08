6750ff19b65caaeff840fb9afedad36926bca9b1
Nelson Tavares de Sousa <stu103017@mail.uni-kiel.de>
2014-07-08 15:27:52 +0200

    Image korrigiert

--- grails-app/domain/hipponade/Image.groovy
+++ grails-app/domain/hipponade/Image.groovy
@@ -2,17 +2,11 @@ package hipponade
 
 class Image {
     String fileName
-    Recipe recipe
-    Event event
-    Product product
 
-    static belongsTo = [Product, Recipe]
+
     static constraints = {
         // Limit upload file size to 2MB
         fileName blank: false, nullable: false
-        recipe nullable: true
-        event nullable: true
-        product nullable: true
     }
 
     def beforeDelete() {
